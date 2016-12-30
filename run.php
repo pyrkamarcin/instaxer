e4a63f20349b558d3f03129d9ae3027f58f67aa6
pyrkamarcin <pyrka.marcin@gmail.com>
2016-12-30 08:49:48 +0100

    Update

--- run.php
+++ run.php
@@ -5,7 +5,7 @@ require __DIR__ . '/config.php';
 
 try {
 
-    $instaxer = new \Instaxer\Instaxer($user1, $pass1, 15, 4);
+    $instaxer = new \Instaxer\Instaxer($user1, $pass1);
     $instaxer->run($array);
 
 } catch (Exception $e) {
