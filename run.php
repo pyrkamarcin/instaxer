8252b5fbb1d41ad37a4f6809ddf12fb8be868210
pyrkamarcin <pyrka.marcin@gmail.com>
2016-12-30 08:45:03 +0100

    Update

--- run.php
+++ run.php
@@ -3,42 +3,10 @@
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/config.php';
 
-$instagram = new \Instagram\Instagram();
-$instagram->setVerifyPeer(false);
-
-$counter = 250;
-$long = 5;
-
-
-$pb = new \ProgressBar\Manager(0, ($counter * $long) - 1, 80, '=', '-', '>');
-$a = 0;
-
 try {
 
-    $instagram->login($user1, $pass1);
-
-    for ($c = 0; $c < $counter; $c++) {
-
-        $item = $array[mt_rand(0, count($array) - 1)];
-
-        $hashTagFeed = $instagram->getTagFeed($item);
-        $items = array_slice($hashTagFeed->getItems(), 0, $long);
-
-        foreach ($items as $hashTagFeedItem) {
-
-            $user = $hashTagFeedItem->getUser();
-            $likeCount = $hashTagFeedItem->getLikeCount();
-            $commentCount = $hashTagFeedItem->getCommentCount();
-
-            if ($hashTagFeedItem->isImage()) {
-                $instagram->likeMedia($hashTagFeedItem->getID());
-                sleep(random_int(2, 5));
-            }
-
-            $pb->update($a++);
-        }
-        sleep(random_int(1, 5));
-    }
+    $instaxer = new \Instaxer\Instaxer($user1, $pass1, 15, 4);
+    $instaxer->run($array);
 
 } catch (Exception $e) {
     echo $e->getMessage() . "\n";
---
+++ src/Instaxer/Instaxer.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Instaxer;
+
+use Instagram\Instagram;
+use ProgressBar\Manager;
+
+/**
+ * Class Instaxer
+ * @package Instaxer
+ */
+class Instaxer
+{
+    /**
+     * @var int
+     */
+    public $counter;
+    /**
+     * @var int
+     */
+    public $long;
+    /**
+     * @var int
+     */
+    public $marker;
+    /**
+     * @var Instagram
+     */
+    private $instagram;
+
+    /**
+     * Instaxer constructor.
+     * @param string $user
+     * @param string $password
+     * @param int $counter
+     * @param int $long
+     * @throws \Exception
+     */
+    public function __construct(string $user, string $password, int $counter = 10, int $long = 2)
+    {
+        $this->instagram = new Instagram();
+        $this->instagram->setVerifyPeer(false);
+
+        $this->counter = $counter;
+        $this->long = $long;
+
+        $this->pb = new Manager(0, ($this->counter * $this->long) - 1, 120, '=', '-', '>');
+        $this->marker = 0;
+
+        $this->instagram->login($user, $password);
+    }
+
+    /**
+     * @param array $array
+     * @throws \Exception
+     */
+    public function run(array $array)
+    {
+        for ($c = 0; $c < $this->counter; $c++) {
+
+            $item = $array[random_int(0, count($array) - 1)];
+
+            $hashTagFeed = $this->instagram->getTagFeed($item);
+            $items = array_slice($hashTagFeed->getItems(), 0, $this->long);
+
+            foreach ($items as $hashTagFeedItem) {
+
+                if ($hashTagFeedItem->isImage()) {
+                    $this->instagram->likeMedia($hashTagFeedItem->getID());
+                    sleep(random_int(2, 5));
+                }
+
+                $this->pb->update($this->marker++);
+            }
+            sleep(random_int(1, 5));
+        }
+    }
+}
