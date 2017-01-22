7610cec4b98c8d7d830a68f067da167312de0807
JetAlone <pyrka.marcin@gmail.com>
2017-01-23 00:15:16 +0100

    Update

--- cleaner.php
+++ cleaner.php
@@ -23,7 +23,6 @@ try {
             if (!empty($instaxer->instagram->searchUsers($username)->getUsers())) {
                 $user = $instaxer->instagram->getUserByUsername($username);
                 echo ' [ ' . $user->getFullName() . ' ] ';
-                echo ' [ ' . $user->getCountryCode() . ' ] ';
                 echo ' [ ' . $instaxer->instagram->unfollowUser($user)->getStatus() . ' ] ' . "\r\n";
             } else {
                 echo ' [ fuck! ] ';
