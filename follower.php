b78486ca606e1c7ca6b4205a17b302a54c988b91
JetAlone <pyrka.marcin@gmail.com>
2017-02-13 22:16:36 +0100

    Update

--- follower.php
+++ follower.php
@@ -18,6 +18,7 @@ try {
     $elements = $hashTagFeed->getItems();
 
     foreach ($elements as $hashTagFeedItem) {
+
         $id = $hashTagFeedItem->getId();
         $user = $instaxer->instagram->getUserInfo($hashTagFeedItem->getUser())->getUser();
 
@@ -28,21 +29,16 @@ try {
                 $userFollow = true;
             }
         }
+
         $file = file_get_contents('storage.tmp');
         $haystack = explode(';', $file);
+
         if (!in_array($user->getUsername(), $haystack, true)) {
             if ($userFollow !== true) {
                 echo $user->getUsername() . ' nie obserwuje mnie' . "\r\n";
                 $instaxer->instagram->followUser($user);
 
-                $hashTagFeed = $instaxer->instagram->getUserFeed($user);
-                $items = array_slice($hashTagFeed->getItems(), 0, random_int(3, 7));
-                foreach ($items as $hashTagFeedItem) {
-                    $instaxer->instagram->likeMedia($hashTagFeedItem->getID());
-                    echo sprintf('[ liked ] ');
-                    sleep(random_int(3, 7));
-                }
-
+                $userInfo = $instaxer->instagram->getUserInfo($user);
 
                 file_put_contents('storage.tmp', $user->getUsername() . ';', FILE_APPEND);
                 sleep(random_int(1, 4));
