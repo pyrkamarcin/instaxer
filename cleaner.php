a160a884017946a35e80d41b8d18f6ae1909b199
JetAlone <pyrka.marcin@gmail.com>
2017-03-03 23:54:41 +0100

    Update

--- cleaner.php
+++ cleaner.php
@@ -1,7 +1,7 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/config.php';
+require __DIR__ . '/jebthecat';
 
 try {
     $path = __DIR__ . '/var/cache/instaxer/profiles/session.dat';
