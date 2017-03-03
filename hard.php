aedd4822136d9d481829acb009abdd6a8da04159
JetAlone <pyrka.marcin@gmail.com>
2017-03-04 00:36:51 +0100

    Update

--- hard.php
+++ hard.php
@@ -44,7 +44,7 @@ try {
                 $userMostImportantStat = $user->getFollowerCount();
 
 
-                if ($userMostImportantStat > 1500) {
+                if ($userMostImportantStat > 5000) {
                     echo $userMostImportantStat . ": \t";
 
                     $instation = new \Instation\Instation($array[1]['username'], $array[1]['password']);
