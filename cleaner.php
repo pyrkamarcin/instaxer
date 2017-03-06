3cfeec60db1b9ff9eab28ab7be8318159a1f0afa
pyrkamarcin <pyrka.marcin@gmail.com>
2017-03-06 07:22:30 +0100

    Update

--- cleaner.php
+++ cleaner.php
@@ -34,7 +34,7 @@ try {
             $file = file_get_contents(__DIR__ . '/storage.tmp');
             $newFile = str_replace($username . ';', '', $file);
             file_put_contents('storage.tmp', $newFile, LOCK_EX);
-            sleep(1);
+            sleep(1, 6);
         }
     }
 
