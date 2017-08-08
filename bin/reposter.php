1a711d1107dacee0b81b57e97eb6e73ae57caf2e
pyrkamarcin <pyrka.marcin>
2017-08-08 07:14:18 +0200

    Update

--- bin/reposter.php
+++ bin/reposter.php
@@ -26,7 +26,7 @@ try {
 
         dump($item->getLikeCount());
 
-        if ($item->getLikeCount() > $avrg * 1.05) {
+        if ($item->getLikeCount() > $avrg * 0.90) {
 
             $image = $item->getImageVersions2()->getCandidates();
             $downloader = new \Instaxer\Downloader();
